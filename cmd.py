19502906b43a34e563fb5e6fc6b2d7c2906dbf2c
yawnoc <leeconway@protonmail.com>
2020-03-28 22:58:11 +0800

    CMD: add function de_indent

--- cmd.py
+++ cmd.py
@@ -28,3 +28,47 @@ Released into the public domain (CC0):
 ABSOLUTELY NO WARRANTY, i.e. "GOD SAVE YOU"
 
 """
+
+
+import re
+from os.path import commonprefix as longest_common_prefix
+
+
+HORIZONTAL_WHITESPACE_REGEX = r'[^\S\n]'
+
+
+def de_indent(string):
+  """
+  De-indent string.
+  
+  Horizontal whitespace at the very start and the very end is removed.
+  Empty lines do not count towards the longest common indentation.
+  
+  By contrast, textwrap.dedent will remove horizontal whitespace
+  from whitespace-only lines even in the middle of the string,
+  which is undesirable.
+  """
+  
+  # Remove horizontal whitespace at the very start and the very end
+  string = re.sub(f'^{HORIZONTAL_WHITESPACE_REGEX}*', '', string)
+  string = re.sub(f'{HORIZONTAL_WHITESPACE_REGEX}*$', '', string)
+  
+  # Get list of all indentations, either
+  # (1) non-empty leading horizontal whitespace, or
+  # (2) the leading empty string on a non-empty line.
+  indentation_list = re.findall(
+    rf'^{HORIZONTAL_WHITESPACE_REGEX}+|^(?=[^\n])',
+    string,
+    flags=re.MULTILINE
+  )
+  
+  # Remove longest common indentation
+  longest_common_indentation = longest_common_prefix(indentation_list)
+  string = re.sub(
+    f'^{longest_common_indentation}',
+    '',
+    string,
+    flags=re.MULTILINE
+  )
+  
+  return string
