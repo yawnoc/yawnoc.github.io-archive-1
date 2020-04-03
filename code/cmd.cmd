33db814b3556a52d3d768c73a05dbb8f0ca152fe
yawnoc <leeconway@protonmail.com>
2020-04-03 17:36:05 +0800

    Add empty CMD page

---
+++ code/cmd.cmd
@@ -0,0 +1,10 @@
+%%
+  %title
+    Python: a regex converter from CMD to HTML
+  %author
+    Conway
+  %description
+    A regex converter from CMD (Conway's markdown) to HTML, written in Python.
+  %date-created
+    2020-04-03
+%%
