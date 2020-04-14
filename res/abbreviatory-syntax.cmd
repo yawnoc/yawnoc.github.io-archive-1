f6d3ab528b2cb636bb0814fbe7fb13f75fe06122
yawnoc <leeconway@protonmail.com>
2020-04-15 01:59:53 +0800

    Fix case of 'X' in BibTeX and BibLaTeX

--- res/abbreviatory-syntax.cmd
+++ res/abbreviatory-syntax.cmd
@@ -39,7 +39,7 @@
     Accessed <span class="js-date">yyyy-mm-dd</span>.
     ----
   
-  * BibTex:
+  * BibTeX:
     <pre><code>\\
       \/@misc{conway-\g<tex_key>,
       \/  author = {Conway},
@@ -50,7 +50,7 @@
       \/}
     </code></pre>
   
-  * BibLaTex:
+  * BibLaTeX:
     <pre><code>\\
       \/@online{conway-\g<tex_key>,
       \/  author = {Conway},
