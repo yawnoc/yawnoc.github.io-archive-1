230e5655b3e01fdb41998f472b4fc82abca15cdc
yawnoc <leeconway@protonmail.com>
2020-04-20 02:16:47 +0800

    Move Tehan on NAPLAN from CCH to CMD

--- cynics-730/2019/sales-tehan-naplan.cmd
+++ cynics-730/2019/sales-tehan-naplan.cmd
@@ -1,247 +1,258 @@
-<*> A cynic's 7.30: [abc.h-short] | 20190830 | 20200119 </*>
+(+ res/abbreviatory-syntax.cmd +)
+(+ res/cynics-730-syntax.cmd +)
 
-<%
-  [abc.h] |
-  Education Minister Dan Tehan discusses NAPLAN and the latest results
-%>
-<%
-  [abc.h-short] |
-  Dan Tehan on NAPLAN (28 Aug 2019)
-%>
-<%
-  [abc.url] |
+%%
+  %title A cynic's 7.30: \short-title
+  %title-suffix \title-suffix
+  %author Conway
+  %date-created 2019-08-30
+  %date-modified 2020-01-19
+  %resources
+    (+ res/main-resources.cmd +)
+  %footer-copyright-remark
+    except the [original interview][original],
+    whose copyright belongs to the ABC
+  %footer-remark
+    [:{Curly-bracketed words}:] inserted by me.
+%%
+
+{: \short-title : Dan Tehan on NAPLAN (28 Aug 2019) :}
+
+[[====
+* \link:home
+* \link:top
+* \link:cynicism
+====]]
+
+
+# %title #
+
+
+||||{page-properties}
+First created: %date-created \\
+Date modified: %date-modified \\
+[Original 7.30 interview][original] ([archived version])
+||||
+
+@@[original]
   https://www.abc.net.au/7.30/\
     education-minister-dan-tehan-discusses-naplan-and/\
     11458690
-%>
-<%
-  [archive.url] |
-  https://web.archive.org/web/20190829130300/[abc.url]
-%>
+@@
 
-<=h>
-  <@ih></@ih>
-  <@it></@it>
-  <@i> More cynicism | /cynics-730/ | A cynic's 7.30 </@i>
-</=h>
+@@[archived version]
+  https://web.archive.org/web/20190829130300/\
+    https://www.abc.net.au/7.30/\
+      education-minister-dan-tehan-discusses-naplan-and/\
+      11458690
+@@
 
-<h1>A cynic's 7.30: [abc.h-short]</h1>
+## Education Minister Dan Tehan discusses NAPLAN and the latest results. ##
 
-<*p></*p>
+{: \leigh-sales : ![LEIGH SALES] :}
+@@![leigh sales]{h}
+  leigh-sales-20190828.jpg
+@@
 
-<p>
-  <b>[abc.h].</b>\
-  <br>
-  <@> Original 7.30 interview | [abc.url] | ABC 7.30: [abc.h] </@>
-  <@> (archived version) | [archive.url] | Web archive: [abc.h] </@>
-</p>
+{: \dan-tehan : ![DAN TEHAN] :}
+@@![dan tehan]{g}
+  dan-tehan-20190828.jpg
+@@
 
-<%
-  [leigh-sales] |
-  <+h> leigh-sales-20190828.jpg | LEIGH SALES </+h>
-%>
-<%
-  [dan-tehan] |
-  <+g> dan-tehan-20190828.jpg | DAN TEHAN </+g>
-%>
-
-<~~>
+||||{interview}
   
-  [leigh-sales]
-  <~h>
+  \leigh-sales
+  ----{h}
     Minister thanks for joining us.
-  </~h>
+  ----
   
-  [dan-tehan]
-  <~g>
+  \dan-tehan
+  ----{g}
     It's a pleasure Leigh.
-  </~g>
+  ----
   
-  [leigh-sales]
-  <~h>
+  \leigh-sales
+  ----{h}
     Federal Government spending on education
     has increased by $20~billion during the past decade,
     with little improvement in results if you look at the NAPLAN outcomes:
     why?
-  </~h>
+  ----
   
-  [dan-tehan]
-  <~g>
+  \dan-tehan
+  ----{g}
     Well, I think it's important for us to understand that
     it's not just about money
-  </~g>
-  <~gt>
-    \{which I'm saying because NAPLAN evidently isn't going so well\}.
-  </~gt>
-  <~g>
+  ----
+  ----{gt}
+    {which I'm saying because NAPLAN evidently isn't going so well}.
+  ----
+  ----{g}
     Obviously money is important
-  </~g>
-  <~gt>
-    \{which I'm saying so that I can quote myself
+  ----
+  ----{gt}
+    {which I'm saying so that I can quote myself
     if Labor attacks us on education spending,
-    and also in case NAPLAN miraculously becomes good later on\},
-  </~gt>
-  <~g>
+    and also in case NAPLAN miraculously becomes good later on},
+  ----
+  ----{g}
     but with that, also has to come reform [...]
-  </~g>
-  <~g>
+  ----
+  ----{g}
     [...] and as we roll this out,
     my hope is that we will start to see an uplift in the NAPLAN results.
-  </~g>
-  <~gt>
-    \{How good are miracles, eh?\}
-  </~gt>
+  ----
+  ----{gt}
+    {How good are miracles, eh?}
+  ----
   
-  [leigh-sales]
-  <~h>
+  \leigh-sales
+  ----{h}
     But you say "as we roll this out"...
     you've been in government for six years now.
-  </~h>
+  ----
   
-  [dan-tehan]
-  <~gt>
-    \{Oh yeah... umm... time for a casual deflection...\}
-  </~gt>
-  <~g>
+  \dan-tehan
+  ----{gt}
+    {Oh yeah... umm... time for a casual deflection...}
+  ----
+  ----{g}
     That's right
     and it takes a while to get all the states and territories onboard. [...]
-  </~g>
+  ----
   
-  [leigh-sales]
-  <~h>
+  \leigh-sales
+  ----{h}
     The three biggest states, Victoria, Queensland and New South Wales,
     are all reviewing NAPLAN.
     Is it a matter of time until it's overhauled or dumped?
-  </~h>
+  ----
   
-  [dan-tehan]
-  <~g>
+  \dan-tehan
+  ----{g}
     So [...] we're transitioning NAPLAN from pen and paper to online. [...]
     It might take us two years, but our hope is that we can get that done
-  </~g>
-  <~gt>
-    \{and this is the same kind of hope as I mentioned before,
-    regarding uplift in results\}.
-  </~gt>
-  <~g>
+  ----
+  ----{gt}
+    {and this is the same kind of hope as I mentioned before,
+    regarding uplift in results}.
+  ----
+  ----{g}
     [...] once we've got NAPLAN fully online,
     it will be time to have a look at NAPLAN and say:
     "OK. How can we make it fit for purpose for the next 5--10 years?"
-  </~g>
-  <~gt>
-    \{I mean, NAPLAN's totally fit for purpose right now, isn't it?\}
-  </~gt>
+  ----
+  ----{gt}
+    {I mean, NAPLAN's totally fit for purpose right now, isn't it?}
+  ----
   
-  [leigh-sales]
-  <~ht>
-    \{Mmm...\}
-  </~ht>
-  <~h>
+  \leigh-sales
+  ----{ht}
+    {Mmm...}
+  ----
+  ----{h}
     On that point about online,
     the last round of tests were marred by computer glitches.
     Are you confident in what the test results are actually showing,
     and do they match your sense of what you see anecdotally [...]?
-  </~h>
+  ----
   
-  [dan-tehan]
-  <~g>
+  \dan-tehan
+  ----{g}
     Yes. All the advice that I'm getting is that the results are comparable,
     and that's come from the experts
-  </~g>
-  <~gt>
-    \{thank God for them\}
-  </~gt>
-  <~g>
+  ----
+  ----{gt}
+    {thank God for them}
+  ----
+  ----{g}
     [...] and [...] the feedback that I'm getting,
     especially when it comes from parents is, that they like NAPLAN
     [...] because they get a sense as to
     where their child's at with their education.
-  </~g>
+  ----
   
-  [leigh-sales]
-  <~h>
+  \leigh-sales
+  ----{h}
     But people were told,
     "Swallow your concerns about, say, pressure on children
     [...], because NAPLAN will help lead to
     significant improvements in student outcomes."
     If it hasn't done that so far, then why keep going with it?
-  </~h>
+  ----
   
-  [dan-tehan]
-  <~g>
+  \dan-tehan
+  ----{g}
     Well, [...] the reason we know that we're getting
     a mixed performance when it comes to NAPLAN
     is because students are undertaking it
-  </~g>
-  <~gt>
-    \{...so if NAPLAN didn't exist then
-    we wouldn't know how bad it is?\}
-  </~gt>
-  <~g>
+  ----
+  ----{gt}
+    {...so if NAPLAN didn't exist then
+    we wouldn't know how bad it is?}
+  ----
+  ----{g}
     [...]
-  </~g>
+  ----
   
-  [leigh-sales]
-  <~h>
+  \leigh-sales
+  ----{h}
     [...] The Chancellor of the University of Queensland Peter Varghese
     raised this question this week, of:
     "What proportion of the student population can be international,
     before you fundamentally change the character of a university?"
-  </~h>
-  <~h>
+  ----
+  ----{h}
     Is it a third? A half? Three quarters?
     What do you think is the answer to that?
-  </~h>
+  ----
   
-  [dan-tehan]
-  <~g>
+  \dan-tehan
+  ----{g}
     Well can I say, I'm very pleased that Peter Varghese as a Chancellor
     is raising this question
-  </~g>
-  <~gt>
-    \{so that I don't have to\}
-  </~gt>
-  <~g>
+  ----
+  ----{gt}
+    {so that I don't have to}
+  ----
+  ----{g}
     [...] and it's something that [...] [university] councils should be asking.
-  </~g>
+  ----
   
-  [leigh-sales]
-  <~ht>
-    \{Well can't you just answer the question?\}
-  </~ht>
-  <~h>
+  \leigh-sales
+  ----{ht}
+    {Well can't you just answer the question?}
+  ----
+  ----{h}
     But as the Minister, I'm asking you what do you think?
     A quarter? A half?
-  </~h>
+  ----
   
-  [dan-tehan]
-  <~g>
+  \dan-tehan
+  ----{g}
     Well Leigh, it varies
-  </~g>
-  <~gt>
-    \{because I don't want to piss off
+  ----
+  ----{gt}
+    {because I don't want to piss off
     people who are filling university coffers,
-    the contents of which eventually flow on into our coffers\}.
-  </~gt>
-  <~g>
+    the contents of which eventually flow on into our coffers}.
+  ----
+  ----{g}
     [...]
-  </~g>
+  ----
   
-  [leigh-sales]
-  <~h>
+  \leigh-sales
+  ----{h}
     Minister, thanks for speaking to us.
-  </~h>
+  ----
   
-  [dan-tehan]
-  <~g>
+  \dan-tehan
+  ----{g}
     Pleasure Leigh.
-  </~g>
+  ----
   
-</~~>
+  \END
+  
+||||
 
-<*f>
-  except the
-  <@> original interview | [abc.url] | ABC 7.30: [abc.h] </@>,
-  whose copyright belongs to the ABC
-  | <:t>\{Curly-bracketed words\}</:t> inserted by me.
-</*f>
+%footer-element
