502477bdc38d5e631d1cd8526915a180da9a5868
yawnoc <leeconway@protonmail.com>
2020-04-21 16:35:09 +0800

    Add custom 404 page

---
+++ 404.cmd
@@ -0,0 +1,35 @@
+(+ res/abbreviatory-syntax.cmd +)
+
+%%
+  %title Page not found
+  %title-suffix \title-suffix
+  %resources
+    (+ res/main-resources.cmd +)
+  %css
+    strong {
+      display: block;
+      margin-bottom: 1em;
+    }
+%%
+
+
+[[====
+* \link:home
+====]]
+
+
+# 404: %title #
+
+----
+**The requested page was not found.**
+
+Head back to the [home page], or see the [sitemap].
+----
+
+@@[home page]
+  /
+@@
+
+@@[sitemap]
+  https://github.com/yawnoc/yawnoc.github.io/blob/master/sitemap.txt
+@@
