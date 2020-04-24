7347cb0c5e37d6646d4252beb84ab4286e5b401d
yawnoc <leeconway@protonmail.com>
2020-04-24 14:30:56 +0800

    Add local fallbacks for CSS and JS
    This is the first working version of fallback resource loading.
    Note currently that different fallback approaches are used:
    * CSS: the existing <link> element is modified to get the local fallback
    * JS: the existing <script> element is left as is,
      and a new <script> is created and appended to get the local fallback
    Modifying the existing JS <script> element does not work.

--- res/maths-resources.cmd
+++ res/maths-resources.cmd
@@ -8,7 +8,12 @@
   href="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css"
   integrity="sha384-\
     zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq"
-  crossorigin="anonymous">
+  crossorigin="anonymous"
+  onerror="
+    this.removeAttribute('integrity');
+    this.removeAttribute('crossorigin');
+    this.removeAttribute('onerror');
+    this.setAttribute('href', '/katex.min.css');">
 
 
 <!--
@@ -22,4 +27,9 @@
   integrity="sha384-\
     y23I5Q6l+B6vatafAwxRu/0oK/79VlbSz7Q9aiSZUvyWYIYsd+qj+o24G5ZU2zJz"
   crossorigin="anonymous"
+  onerror="
+    let fallbackMathsJS = document.createElement('script');
+    fallbackMathsJS.setAttribute('src', '/katex.min.js');
+    fallbackMathsJS.setAttribute('onload', 'renderMaths()');
+    document.head.appendChild(fallbackMathsJS);"
   onload="renderMaths()"></script>
