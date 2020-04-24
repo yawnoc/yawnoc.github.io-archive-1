610c3ebe6fe19aa0010c84f0db5c039e163b8525
yawnoc <leeconway@protonmail.com>
2020-04-24 12:26:44 +0800

    Add rendering test (currently needs CDN)

---
+++ code/sandbox.cmd
@@ -0,0 +1,29 @@
+(+ res/abbreviatory-syntax.cmd +)
+
+%%
+  %title Sandbox
+  %title-suffix \title-suffix
+  %author Conway
+  %date-created 2020-04-xx
+  %date-modified 2020-04-xx
+  %resources
+    (+ res/main-resources.cmd +)
+    (+ res/rendering-js.cmd +)
+    (+ res/maths-resources.cmd +)
+%%
+
+[[====
+* \link:home
+====]]
+
+# Sandbox #
+
+$$
+  1 + \frac{1}{2^2} + \frac{1}{3^2} + \dots
+  = \frac{\pi^2}{6}
+  < 2
+$$
+
+----
+And, he saith, $$$ \text{Yea, \$$d$ meaneth $d$~dollars} $$$.
+----
