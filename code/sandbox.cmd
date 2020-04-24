f69069177ef5b8c515fd1393558ccc03dd8c874e
yawnoc <leeconway@protonmail.com>
2020-04-25 02:25:30 +0800

    Add <noscript> for equations to sandbox

--- code/sandbox.cmd
+++ code/sandbox.cmd
@@ -4,8 +4,8 @@
   %title Sandbox
   %title-suffix \title-suffix
   %author Conway
-  %date-created 2020-04-xx
-  %date-modified 2020-04-xx
+  %date-created 2020-04-24
+  %date-modified 2020-04-25
   %resources
     (+ res/main-resources.cmd +)
     (+ res/rendering-javascript.cmd +)
@@ -17,6 +17,11 @@
 ====]]
 
 # Sandbox #
+
+\noscript[]
+
+[||||
+||||]
 
 $$
   1 + \frac{1}{2^2} + \frac{1}{3^2} + \dots
