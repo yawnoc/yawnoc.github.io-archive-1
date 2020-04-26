8152add686b8ba4e876d190cdb38c65a9c548365
yawnoc <leeconway@protonmail.com>
2020-04-27 02:17:10 +0800

    Add plan for accessible inline SVGs

--- res/general-syntax.cmd
+++ res/general-syntax.cmd
@@ -19,6 +19,7 @@
   Romanisation special characters
   Automatic language declarations for Chinese
   SVG embedded styles
+  Accessible inline SVGs
   
 -->
 
@@ -589,4 +590,84 @@
 %
   <svg class="embedded-styles"><style>\g<content></style></svg>
 %}
+
+
+
+<!--
+  ----------------------------------------------------------------
+  Accessible inline SVGs
+  ----------------------------------------------------------------
+  Inline SVGs do not have an equivalent of <img>'s alt attribute,
+  which is really dumb.
+  
+  Basically how a screenreader treats an SVG
+  will depend on the screenreader and the browser.
+  
+  Reading:
+  
+  * Heather Migliorisi (2016) for CSS-Tricks
+    "Accessible SVGs"
+    https://css-tricks.com/accessible-svgs/
+  
+  * Scott O'Hara (2019)
+    "Contextually Marking up accessible images and SVGs"
+    https://www.scottohara.me/blog/2019/05/22/\
+      contextual-images-svgs-and-a11y.html
+  
+  * Scott O'Hara (2019)
+    "SVG and Image markup tests"
+    https://scottaohara.github.io/testing/img-svg-acc-name/img-svg-tests.html
+    https://web.archive.org/web/20200426173932/{ibid}
+  
+  From Heather's article:
+    """"
+    According to the W3C specification,
+    we shouldn’t have to do anything extra for SVGs
+    beyond providing the `<title>` and possibly a `<desc>`
+    because they should be available to the Accessibility API.
+    Unfortunately, browser support is not quite there yet
+    (bugs reported for: Chrome and Firefox).
+    
+    [...] Add the appropriate ID’s to the `<title>` and `<desc>` [...]
+    
+    [...] On the `<svg>` tag, add:
+      `aria-labelledby="uniqueTitleID uniqueDescID"` [...]
+    
+    [...] On the `<svg>` tag, add:
+      `role="img"` [...]
+    """"
+  Note that that article was last updated in 2016.
+  
+  From Scott's "SVG and Image markup tests",
+  written in 2019 and accessed 2020-04-27,
+  Test 11 is the most robust in terms of accessibility for inline SVGs
+  (all tests pass but three exceptions for JAWS on IE11).
+  
+  The pattern for Test 11 is thus:
+  ````
+  <svg viewBox="..." height="..." width="..."
+    role="img"
+    focusable="false"
+    aria-labelledby="id"
+  >
+    <title id="id">Accessible Name</title>
+    <path aria-hidden="true" d="..."></path>
+  </svg>
+  ````
+  
+  Therefore I shall define the following:
+  
+  * \accessible-svg-attributes[{id}]
+    role="img" focusable="false" aria-labelledby="{id}"
+  
+  * [accessible-svg-title/{id}  {content}  /]
+    <title id="{id}">{content}</title>
+  
+  * \accessible-svg-aria
+    aria-hidden="true"
+  
+  If the labelling by id ever becomes unnecessary,
+  then I will remove the {id} bits.
+  
+-->
 
