67dc6d37bf9ffae048a6214efdc646582a04b440
yawnoc <leeconway@protonmail.com>
2020-04-30 23:42:02 +0800

    Add syntax for bracketed original Chinese

--- res/general-syntax.cmd
+++ res/general-syntax.cmd
@@ -18,6 +18,7 @@
   Romanisation spans  [^ ]  [_ ]
   Romanisation special characters
   Automatic language declarations for Chinese
+  Bracketed original Chinese for romanisations  (( ))
   SVG embedded styles
   Accessible inline SVGs
   
@@ -534,6 +535,39 @@
   )+
 %
   <span lang="zh-Hant">\g<0></span>
+%}
+
+
+
+<!--
+  ----------------------------------------------------------------
+  Bracketed original Chinese after romanisations  (( ))
+  ----------------------------------------------------------------
+  See "Automatic language declarations for Chinese" above
+  for what is regarded as a Chinese character.
+  Note that the brackets are not a part of the HTML,
+  but are added cosmetically via CSS and are therefore not selectable.
+  Replaces
+    {leading whitespace} ((<span lang="zh-Hant">{content}</span>))
+  with
+    ~<span lang="zh-Hant" class="original-chinese">{content}</span>
+  so that the match must be split before the first >.
+-->
+
+{%
+  [\s] *
+  \(\(
+    (?P<match_portion_1>
+      <span[ ]lang="zh-Hant"
+    )
+    (?P<match_portion_2>
+      >
+        [\s\S]*?
+      </span>
+    )
+  \)\)
+%
+  ~\g<match_portion_1> class="original-chinese"\g<match_portion_2>
 %}
 
 
