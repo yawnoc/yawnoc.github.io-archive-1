d07978ed557bceb2ce5d8bc9aace2bd415a61318
yawnoc <leeconway@protonmail.com>
2020-05-06 13:44:23 +0800

    Add Sun Tzu template for testing

---
+++ sun-tzu/template.cmd
@@ -0,0 +1,23 @@
+(+ res/general-syntax.cmd +)
+
+%%
+  %title ~~ "\sun-tzu's Computational Classic" 《孫子算經》 ~~
+  %title-en \sun-tzu's Computational Classic"
+  %title-suffix \title-suffix
+  %author Conway
+  %date-created 2020-05-06
+  %date-modified 2020-05-06
+  %resources
+    (+ res/main-resources.cmd +)
+%%
+
+[[====
+* \link:home
+* \link:top
+* \link:cite
+====]]
+
+#
+  Template for 《孫子算經》 \+
+  %title-en
+#
