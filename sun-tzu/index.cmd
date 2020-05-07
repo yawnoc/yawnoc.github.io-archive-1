7dad5a3611e14a30e8bffdba7c735ff495a0b9ee
yawnoc <leeconway@protonmail.com>
2020-05-07 17:34:59 +0800

    Create /sun-tzu/index.cmd

---
+++ sun-tzu/index.cmd
@@ -0,0 +1,69 @@
+(+ res/general-syntax.cmd +)
+
+%%
+  %title "\sun-tzu's Computational Classic" ~~《孫子算經》~~
+  %title-en "\sun-tzu's Computational Classic"
+  %title-suffix \title-suffix
+  %author Conway
+  %date-created 2019-05-04
+  %date-modified 2020-05-xx
+  %resources
+    (+ res/main-resources.cmd +)
+    (+ res/rendering-javascript.cmd +)
+  %css
+    .sun-tzu-navigation {
+      font-size: large;
+    }
+%%
+
+[[====
+* \header-link:home
+* \header-link:top
+* [\sun-tzu](\/ You are viewing %title-en)
+*> [Pref.] ["p" Preface]
+* [I] ["i" Volume~I]
+* [II] ["ii" Volume~II]
+<!-- * [III] ["iii" Volume~III] -->
+* \header-link:cite
+====]]
+
+#
+  《孫子算經》 \+
+  %title-en
+#
+
+
+[||||
+Also known as _Sunzi Suanjing_.
+||||]
+
+
+##translation
+  Translation (links to volumes)
+##
+
+<<
+  《孫子算經》
+||
+  "\sun-tzu's Computational Classic"
+>>
+
+===={sun-tzu-navigation}
+* \link:sun-tzu/preface
+* \link:sun-tzu/i
+* \link:sun-tzu/ii
+* \link:sun-tzu/iii
+====
+
+
+##conways-preface
+  Conway's Preface
+##
+
+
+
+
+\cite-this-page[%title-en][sun-tzu][
+  ~~``{Sun}~{Tz{\uu}'s} {Computational} {Classic}''~~
+]
+
