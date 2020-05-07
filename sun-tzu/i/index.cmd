1d2b2de3e7cf8419f90d5aa01fb3a3e727651f40
yawnoc <leeconway@protonmail.com>
2020-05-08 00:14:19 +0800

    Create /sun-tzu/i/index

---
+++ sun-tzu/i/index.cmd
@@ -0,0 +1,49 @@
+(+ res/general-syntax.cmd +)
+
+%%
+  %title "\sun-tzu's Computational Classic: Volume~I" ~~《孫子算經・卷上》~~
+  %title-en "\sun-tzu's Computational Classic: Volume~I"
+  %title-suffix \title-suffix
+  %author Conway
+  %date-created 2019-06-09
+  %date-modified 2020-05-xx
+  %resources
+    (+ res/main-resources.cmd +)
+    (+ res/rendering-javascript.cmd +)
+%%
+
+[[====
+* \header-link:home
+* \header-link:top
+* \header-link:sun-tzu
+*> [Vol.~I](\/ You are viewing Volume~I)
+* \header-link:cite
+====]]
+
+
+#
+  《孫子算經\.卷上》 \+
+  %title-en
+#
+
+
+[||||
+Literally "Upper Volume".
+||||]
+
+
+
+{{{{|
+  \nav-up: \sun-tzu's Computational Classic : [""]
+  {{|
+    \nav-prev: Pref. : ["p"]
+    \nav-curr: Volume~I
+    \nav-next: Vol.~II : ["ii"]
+  |}}
+|}}}}
+
+
+\cite-this-page[%title-en][sun-tzu-i][
+  ~~``{Sun}~{Tz{\uu}'s} {Computational} {Classic}: {Volume}~{I}''~~
+]
+
